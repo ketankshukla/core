0223c42e51a502353072a36e24a16470b1ded987
Ketan Shukla <ketankshukla@gmail.com>
2024-07-19 11:29:51 -0700

    created Book and BookTitle models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils.text import slugify
+
+from publishers.models import Publisher
+from authors.models import Author
+import uuid
+
 
 # Create your models here.
+class BookTitle(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Book Position {self.title}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+
+class Book(models.Model):
+    title = models.ForeignKey(BookTitle, on_delete=models.CASCADE)
+    book_id = models.CharField(max_length=24, blank=True)
+    # QR code
+    qr_code = models.ImageField(upload_to="qr_codes", blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        if not self.book_id:
+            self.book_id = str(uuid.uuid4().replace('-', '')[:24].lower())
+
+        # generate QR code
+
+        return super().save(*args, **kwargs)
+
