3ce2727a7a59587f592e53fbec0e32673709c3ed
Ketan Shukla <ketankshukla@gmail.com>
2024-07-19 10:51:28 -0700

    changed id to uuid for authors app

--- authors/models.py
+++ authors/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Author(models.Model):
     Book author class
     Managed only in django admin
     '''
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
 
     def __str__(self):
